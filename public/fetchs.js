639cfb3a7e921be53854502eefaa9e6a4c2adf08
brzenit <bryan.lima@sptech.school>
2024-10-02 18:05:03 -0300

    login com bug

--- public/fetchs.js
+++ public/fetchs.js
@@ -1,7 +1,74 @@
+
+function irDash() {
+  window.location = "./dashboard/dashboard.html#dashboard";
+}// apenas para exibir o loading
+
+
+  function entrar() {
+    var emailVar = emailLogin.value;
+    let senhaVar = senhaLogin.value;
+
+    if (emailVar == "" || senhaVar == "") {
+        console.log("Mensagem de erro para todos os campos em branco");
+        return false;
+    }
+    else {
+      console.log("Sumir Mensagem?")
+    }
+
+    console.log("FORM LOGIN: ", emailVar);
+    console.log("FORM SENHA: ", senhaVar);
+
+    fetch("/usuarios/autenticar", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            emailServer: emailVar,
+            senhaServer: senhaVar
+        })
+    }).then(function (resposta) {
+        console.log("ESTOU NO THEN DO entrar()! RESPOSTA ABAIXO")
+      console.log(resposta)
+        if (resposta.ok) {
+            console.log(resposta);
+
+            resposta.json().then(json => {
+              console.log("Dentro do resposta.json")
+                console.log(json);
+                console.log(JSON.stringify(json));
+                json.forEach(element => {
+                  console.log(element.nome)                
+                
+                sessionStorage.EMAIL_USUARIO = json.email;
+                sessionStorage.TIPO_USUARIO = json.tipo
+                // sessionStorage.NOME_USUARIO = json.nome;
+                sessionStorage.ID_USUARIO = json.id;
+                // sessionStorage.AQUARIOS = JSON.stringify(json.aquarios)
+
+              });
+                irDash();
+
+            });
+            irDash();
+        } else {
+
+            console.log("Houve um erro ao tentar realizar o login!");
+
+            resposta.text().then(texto => {
+                console.error(texto);
+            });
+        }
+
+    }).catch(function (erro) {
+        console.log(erro);
+    })
+
+    return false;
+}
+
 function cadastrar() {
-    
-
-
     //Recupere o valor da nova input pelo nome do id
     // Agora vá para o método fetch logo abaixo
     let emailVar = emailCadastro.value
@@ -61,63 +128,14 @@ function cadastrar() {
   }
 
    // Listando empresas cadastradas 
-   function entrar() {
-    var emailVar = emailLogin.value;
-    let senhaVar = senhaLogin.value;
+   
 
-    if (emailVar == "" || senhaVar == "") {
-        console.log("Mensagem de erro para todos os campos em branco");
-        return false;
-    }
-    else {
-      console.log("Sumir Mensagem?")
-    }
 
-    console.log("FORM LOGIN: ", emailVar);
-    console.log("FORM SENHA: ", senhaVar);
+function deletarUser(){
 
-    fetch("/usuarios/autenticar", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            emailServer: emailVar,
-            senhaServer: senhaVar
-        })
-    }).then(function (resposta) {
-        console.log("ESTOU NO THEN DO entrar()!")
-
-        if (resposta.ok) {
-            console.log(resposta);
-
-            resposta.json().then(json => {
-                console.log(json);
-                console.log(JSON.stringify(json));
-                sessionStorage.EMAIL_USUARIO = json.email;
-                sessionStorage.TIPO_USUARIO = json.tipo
-                // sessionStorage.NOME_USUARIO = json.nome;
-                sessionStorage.ID_USUARIO = json.id;
-                // sessionStorage.AQUARIOS = JSON.stringify(json.aquarios)
-
-                setTimeout(function () {
-                    window.location = "./dashboard/dashboard.html#dashboard";
-                }, 1000); // apenas para exibir o loading
-
-            });
-
-        } else {
-
-            console.log("Houve um erro ao tentar realizar o login!");
-
-            resposta.text().then(texto => {
-                console.error(texto);
-            });
-        }
-
-    }).catch(function (erro) {
-        console.log(erro);
-    })
-
-    return false;
 }
+
+function atualizarUser(){
+
+}
+
--- src/models/usuarioModel.js
+++ src/models/usuarioModel.js
@@ -1,7 +1,7 @@
 var database = require("../database/config")
 
 function autenticar(email, senha) {
-    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", email, senha)
+    console.log(" \n\n function entrar(): ", email, senha)
     var instrucaoSql = `
         SELECT * FROM usuario WHERE email = '${email}' AND senha = '${senha}';
     `;
