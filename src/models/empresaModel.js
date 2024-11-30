315a0d25cc6859692d9ccd79cb20b55529cd1a2a
enricoferrer <enricoferrer@outlook.com>
2024-11-30 18:18:38 -0300

    AGORA VAI3

--- src/models/empresaModel.js
+++ src/models/empresaModel.js
@@ -1,7 +1,7 @@
 var database = require("../database/config");
 
 function buscar(id) {
-  var instrucaoSql = `SELECT empresa.idEmpresa, endereco.cep, endereco.cidade, empresa.nomeFantasia, empresa.cnpj, empresa.status FROM endereco inner join empresa on endereco.idEndereco = empresa.fk_endereco where empresa.fk_usuario = '${id}'` ;
+  var instrucaoSql = `SELECT empresa.idEmpresa, endereco.cep, endereco.cidade, empresa.nomeFantasia, empresa.cnpj, empresa.status FROM endereco inner join empresa on endereco.idEndereco = empresa.fk_endereco where empresa.fk_usuario = '${id}' and empresa.status = "Ativo"` ;
 
   return database.executar(instrucaoSql);
 }
